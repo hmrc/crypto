2a8d82c32e46e094724ebab0f1d01c6b1badde8f
MoDweik <180322631+MoDweik@users.noreply.github.com>
2025-09-29 22:12:27 +0100

    refactor

--- project/LibDependencies.scala
+++ project/LibDependencies.scala
@@ -3,18 +3,18 @@ import sbt._
 object LibDependencies {
 
   val cryptoCompile = Seq(
-    "com.typesafe"     %  "config"         % "1.4.3",
+    "com.typesafe"     %  "config"         % "1.4.5",
     "javax.inject"     %  "javax.inject"   % "1"
   )
 
   val cryptoTest = Seq(
-    "org.scalatest"        %% "scalatest"       % "3.2.17"   % Test,
+    "org.scalatest"        %% "scalatest"       % "3.2.19"   % Test,
     "com.vladsch.flexmark" %  "flexmark-all"    % "0.64.8"   % Test,
-    "org.scalatestplus"    %% "scalacheck-1-17" % "3.2.17.0" % Test,
-    "org.scalatestplus"    %% "mockito-4-11"    % "3.2.17.0" % Test
+    "org.scalatestplus"    %% "scalacheck-1-17" % "3.2.18.0" % Test,
+    "org.scalatestplus"    %% "mockito-4-11"    % "3.2.18.0" % Test
   )
 
   val cryptoJsonPlay30Compile = Seq(
-    "org.playframework" %% "play-json" % "3.0.5" // version provided by Play 3.0
+    "org.playframework" %% "play-json" % "3.0.9" // version provided by Play 3.0
   )
 }
